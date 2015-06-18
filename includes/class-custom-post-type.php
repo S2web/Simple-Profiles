4f003bb89b643008198c560ce68c7a4ce404c0e7
Steven Slack <steven@s2webpress.com>
2015-06-18 00:02:51 -0400

    removed the no-cart reference

--- includes/class-custom-post-type.php
+++ includes/class-custom-post-type.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Register all the custom post types and taxonomies
- * 
+ *
  * @link      http://s2webpress.com
  * @since     1.0.0
  *
@@ -19,10 +19,10 @@ class S2_Profiles_CPT {
 	 */
 	public function __construct() {
 
-		// register the nocart post type
+		// register the post type
 		add_action( 'init', array( $this, 'register_cpt' ) );
 
-		// register the nocart taxonomy
+		// register the taxonomy
 		add_action( 'init', array( $this, 'register_tax' ) );
 
 	}
@@ -30,28 +30,28 @@ class S2_Profiles_CPT {
 	/**
 	 * Register s2-profiles custom post type
 	 * http://codex.wordpress.org/Function_Reference/register_post_type
-	 * 
+	 *
 	 */
 	public function register_cpt() {
 
 		add_image_size( 'profiles-thumb', 300, 300, true ); // adds the profile image size
 
 		$labels = array(
-			'name'               => __( 'Profiles', 'simple-profiles' ), 
-			'singular_name'      => __( 'Profile', 'simple-profiles' ), 
-			'all_items'          => __( 'All Profiles', 'simple-profiles' ), 
-			'add_new'            => __( 'Add New Profile', 'simple-profiles' ), 
+			'name'               => __( 'Profiles', 'simple-profiles' ),
+			'singular_name'      => __( 'Profile', 'simple-profiles' ),
+			'all_items'          => __( 'All Profiles', 'simple-profiles' ),
+			'add_new'            => __( 'Add New Profile', 'simple-profiles' ),
 			'add_new_item'       => __( 'Add New Profile', 'simple-profiles' ),
-			'edit'               => __( 'Edit Profile', 'simple-profiles' ), 
-			'edit_item'          => __( 'Edit Profile', 'simple-profiles' ), 
-			'new_item'           => __( 'New Profile', 'simple-profiles' ), 
-			'view_item'          => __( 'View Profile', 'simple-profiles' ), 
-			'search_items'       => __( 'Search Profiles', 'simple-profiles' ), 
-			'not_found'          => __( 'Nothing found. Try creating a new Profile.', 'simple-profiles' ), 
+			'edit'               => __( 'Edit Profile', 'simple-profiles' ),
+			'edit_item'          => __( 'Edit Profile', 'simple-profiles' ),
+			'new_item'           => __( 'New Profile', 'simple-profiles' ),
+			'view_item'          => __( 'View Profile', 'simple-profiles' ),
+			'search_items'       => __( 'Search Profiles', 'simple-profiles' ),
+			'not_found'          => __( 'Nothing found. Try creating a new Profile.', 'simple-profiles' ),
 			'not_found_in_trash' => __( 'Nothing found in Trash', 'simple-profiles' ),
 			'parent_item_colon'  => ''
 		);
-			
+
 		$args = array(
 			'labels'                => $labels,
 			'public' 				=> true,
@@ -62,9 +62,9 @@ class S2_Profiles_CPT {
 			'query_var'			 	=> true,
 			'menu_position' 		=> 20,
 			'hierarchical'			=> false,
-			'menu_icon'				=>'dashicons-groups',	
-			'rewrite'				=> array( 'slug' => 'profiles', 'with_front' => false ), 	
-			'has_archive' 			=> true, 
+			'menu_icon'				=>'dashicons-groups',
+			'rewrite'				=> array( 'slug' => 'profiles', 'with_front' => false ),
+			'has_archive' 			=> true,
 			'capability_type' 		=> 'page',
 			'supports' 				=> array( 'title', 'editor', 'thumbnail', 'page-attributes'  ),
 		);
@@ -77,21 +77,21 @@ class S2_Profiles_CPT {
 	 */
 	public function register_tax() {
 
-		// Adds Category taxonomy for the Showcase Catalog Custom Post Type
+		// Adds Category taxonomy for the Profiles Custom Post Type
 		$labels = array(
-			'name'              => _x( 'Profile Category', 'taxonomy general name', 'sc-catalog' ),
-			'singular_name'     => _x( 'Profile Category', 'taxonomy singular name', 'sc-catalog' ),
-			'search_items'      => __( 'Search Profile Categories', 'sc-catalog' ),
-			'all_items'         => __( 'All Profile Categories', 'sc-catalog' ),
-			'parent_item'       => __( 'Parent Profile Category', 'sc-catalog' ),
-			'parent_item_colon' => __( 'Parent Profile Category:', 'sc-catalog' ),
-			'edit_item'         => __( 'Edit Profile Category', 'sc-catalog' ),
-			'update_item'       => __( 'Update Profile Category', 'sc-catalog' ),
-			'add_new_item'      => __( 'Add New Profile Category', 'sc-catalog' ),
-			'new_item_name'     => __( 'New Profile Category Name', 'sc-catalog' ),
-			'menu_name'         => __( 'Profile Categories', 'sc-catalog' ),
+			'name'              => _x( 'Profile Category', 'taxonomy general name', 'simple-profiles' ),
+			'singular_name'     => _x( 'Profile Category', 'taxonomy singular name', 'simple-profiles' ),
+			'search_items'      => __( 'Search Profile Categories', 'simple-profiles' ),
+			'all_items'         => __( 'All Profile Categories', 'simple-profiles' ),
+			'parent_item'       => __( 'Parent Profile Category', 'simple-profiles' ),
+			'parent_item_colon' => __( 'Parent Profile Category:', 'simple-profiles' ),
+			'edit_item'         => __( 'Edit Profile Category', 'simple-profiles' ),
+			'update_item'       => __( 'Update Profile Category', 'simple-profiles' ),
+			'add_new_item'      => __( 'Add New Profile Category', 'simple-profiles' ),
+			'new_item_name'     => __( 'New Profile Category Name', 'simple-profiles' ),
+			'menu_name'         => __( 'Profile Categories', 'simple-profiles' ),
 		);
-			
+
 		$args = array(
 			'hierarchical'      => true,
 			'labels'            => $labels,
@@ -99,7 +99,7 @@ class S2_Profiles_CPT {
 			'show_in_nav_menus' => true,
 			'show_admin_column' => true,
 			'query_var'         => true,
-			'rewrite'           => array( 'slug' => 'profile-category', 'with_front' => false ), 
+			'rewrite'           => array( 'slug' => 'profile-category', 'with_front' => false ),
 		);
 
 		register_taxonomy( 's2_profiles_cat', array( 's2_profiles' ), $args );
